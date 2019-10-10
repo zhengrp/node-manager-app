b646a497f2092e662fc7f431f4e5b5caa3afdfe0
zhengrp <18298377115@139.com>
2019-10-10 15:56:32 +0800

    make it better

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+// 开发环境连接
+module.exports ={
+    mongoURI:'mongodb+srv://test:test1234@cluster0-gr5iy.mongodb.net/test?retryWrites=true&w=majority',
+    // mongoURI: 'mongodb://nodevueelement:test1234@ds223756.mlab.com:23756/node-vue-element',
+    secretOrKey:'secret'
+}
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+// 开发环境连接
+module.exports ={
+    mongoURI:process.env.MONGO_URL,
+    secretOrKey:process.env.SECRE_OR_KEY
+}
